a1c5438854844ea6d1f2fc510dce680cdfc37be6
vlad <vlad@vladmarin.ro>
2021-08-05 14:47:09 +0300

    pics resolutions changed

--- script.js
+++ script.js
@@ -31,13 +31,13 @@ $(document).ready(function(){
     });
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["HTML", "CSS", "Javascript", "NodeJS", "SQL"],
+        strings: ["HTML", "CSS", "Javascript", "NodeJS", "SQL", "jQuery", "Bootstrap"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
     var typed = new Typed(".typing-2", {
-        strings: ["HTML", "CSS", "Javascript", "NodeJS", "SQL"],
+        strings: ["HTML", "CSS", "Javascript", "NodeJS", "SQL", "jQuery", "Bootstrap"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
